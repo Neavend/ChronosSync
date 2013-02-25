49f6fa8c258506a21784f295f412a6258f2c06f5
Anthony Seure <anthony.seure@gmail.com>
2013-02-25 13:52:04 +0100

    Clean old Google agenda events

--- getchronos.py
+++ getchronos.py
@@ -22,16 +22,13 @@ from oauth2client.tools import run
 def startTime(rawHour):
   hours = int(rawHour) / 4
   minutes = (int(rawHour) % 4) * 15
-
   if (hours < 10):
     final = "0" + str(hours) + ":"
   else:
     final = str(hours) + ":"
-
   if (minutes < 10):
     final += "0"
   final += str(minutes) + ":00.000"
-
   return final
 
 #============================================
@@ -48,7 +45,6 @@ def getStart(rawDate, rawHour):
   final = year + "-" + month + "-" + day + "T"
   final += startTime(rawHour)
   final += "+01:00"
-  #print final
   return final
 
 #================================================
@@ -61,63 +57,76 @@ def getEnd(rawDate, rawHour, rawDuration):
   final = year + "-" + month + "-" + day + "T"
   final += endTime(rawHour, rawDuration)
   final += "+01:00"
-  #print final
   return final
 
-#==============================
-# Read JSON configuration file
-#==============================
-with open('csi.json') as f:
-  conf = json.load(f)
 
-  auth = conf['chronos']['auth']
-  num = conf['chronos']['num']
-  week = conf['chronos']['week']
-  group = conf['chronos']['group']
 
-  cid = conf['google-calendar']['cid']
-  cs = conf['google-calendar']['cs']
-  scope = conf['google-calendar']['scope']
-  calid = conf['google-calendar']['calid']
 
-#=========================
-# Getting the Chronos XML
-#=========================
-chronosXML = urllib.urlopen('http://webservices.chronos.epita.net/GetWeeks.aspx?'
-                            + 'auth=' + auth + '&num=' + num
-                            + '&week=' + week + '&group=' + group)
-tree = ET.parse(chronosXML)
-root = tree.getroot()
 
-#=======================================
-# Getting and formating all the courses
-#=======================================
-courses = []
-for day in root.iter('day'):
-  rawDate = day.find('date').text
-  for course in day.iter('course'):
-    if course.find('title') != None:
-      rawTitle = course.find('title').text
-    if course.find('hour') != None:
-      rawHour = course.find('hour').text
-    if course.find('duration') != None:
-      rawDuration = course.find('duration').text
-    if course.find('instructor') != None:
-      rawTeacher = course.find('instructor').text
-    if course.find('room') != None:
-      rawRoom = course.find('room').text
-    courses.append([
-      getStart(rawDate, rawHour),
-      getEnd(rawDate, rawHour, rawDuration),
-      rawTitle,
-      rawRoom,
-      rawTeacher
-      ])
 
-#======
+
+#===============================================================================
 # MAIN
-#======
+#===============================================================================
 def main():
+  print '-- ChronosSync starting'
+  #==============================
+  # Read JSON configuration file
+  #==============================
+  print 'Reading configuration file...'
+  with open('csi.json') as f:
+    conf = json.load(f)
+
+    auth = conf['chronos']['auth']
+    num = conf['chronos']['num']
+    week = conf['chronos']['week']
+    group = conf['chronos']['group']
+
+    cid = conf['google-calendar']['cid']
+    cs = conf['google-calendar']['cs']
+    scope = conf['google-calendar']['scope']
+    calid = conf['google-calendar']['calid']
+
+  #=========================
+  # Getting the Chronos XML
+  #=========================
+  print 'Recovering Chronos timetables...'
+  chronosXML = urllib.urlopen('http://webservices.chronos.epita.net/GetWeeks.aspx?'
+                              + 'auth=' + auth + '&num=' + num
+                              + '&week=' + week + '&group=' + group)
+  tree = ET.parse(chronosXML)
+  root = tree.getroot()
+
+  #=======================================
+  # Getting and formating all the courses
+  #=======================================
+  print 'Formating courses...'
+  courses = []
+  for day in root.iter('day'):
+    rawDate = day.find('date').text
+    for course in day.iter('course'):
+      if course.find('title') != None:
+        rawTitle = course.find('title').text
+      if course.find('hour') != None:
+        rawHour = course.find('hour').text
+      if course.find('duration') != None:
+        rawDuration = course.find('duration').text
+      if course.find('instructor') != None:
+        rawTeacher = course.find('instructor').text
+      if course.find('room') != None:
+        rawRoom = course.find('room').text
+      courses.append([
+        getStart(rawDate, rawHour),
+        getEnd(rawDate, rawHour, rawDuration),
+        rawTitle,
+        rawRoom,
+        rawTeacher
+        ])
+
+  #===================================
+  # Connecting to Google Calendar API
+  #===================================
+  print 'Connection to Google Calendar...'
   flow = OAuth2WebServerFlow(cid, cs, scope)
   storage = Storage('credentials.dat')
   credentials = storage.get()
@@ -128,6 +137,24 @@ def main():
   service = build('calendar', 'v3', http=http)
 
   try:
+    #============================================
+    # Cleaning old events from the Google Agenda
+    #============================================
+    print 'Cleaning Google Agenda...'
+    page_token = None
+    while True:
+      events = service.events().list(calendarId=calid, pageToken=page_token).execute()
+      if events['items']:
+        for event in events['items']:
+          service.events().delete(calendarId=calid, eventId=event['id']).execute()
+      page_token = events.get('nextPageToken')
+      if not page_token:
+        break
+
+    #===========================
+    # Uploding on Google Agenda
+    #===========================
+    print 'Uploading courses on Google Calendar...'
     for c in courses:
       event = {
         'start': {
@@ -142,6 +169,7 @@ def main():
       }
       created_event = service.events().insert(calendarId=calid, body=event).execute()
 
+    print '-- Goodbye'
   except AccessTokenRefreshError:
     print ('Credentials have been revoked')
 
